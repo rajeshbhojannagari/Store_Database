3353ce2c8ca4100adaa64e94393a9ea0b7e84ce1
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-02-24 00:14:09 +0530

    making chnages

--- store_management/sales/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import OrderItem
-
-class OrderItemForm(forms.ModelForm):
-    class Meta:
-        model=OrderItem
-        fields=['product','quantity']
--- store_management/sales/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns=[
-    path('',views.product_catalog,name='product_catalog'),
-    path('order/',views.create_order,name='create_order'),
-    path('success/',views.order_success,name='order_success')
-]
